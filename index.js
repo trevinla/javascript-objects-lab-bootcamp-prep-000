da9635a769835368457399d5d9e90b91afad6cb6
Trevor Seitz <trevinla@gmail.com>
2017-10-05 20:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {};
 
+<<<<<<< HEAD
 function updateObjectWithKeyAndValue(recipes, ingredient, value) {
   //recipes[ingredient] = value;
   return Object.assign({}, recipes, {[ingredient]: value});
@@ -22,3 +23,21 @@ function destructivelyDeleteFromObjectByKey(recipes, ingredient) {
   delete recipes[ingredient];
   return recipes;
 }
+=======
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function UpdateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({key: value}, object);
+
+  return newObj;
+}
+
+
+// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//   var object = Object.assign({}, object):
+//   return object;
+// }
+>>>>>>> 60bc3e7e245c991df48931b9413965c28323c572
